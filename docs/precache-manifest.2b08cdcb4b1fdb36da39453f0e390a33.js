e57201d1bbfffc1116aecbdbfb63199f3ec6ca0e
masterpar <camilo.c.carranza@gmail.com>
2020-08-28 11:07:15 -0500

    docs camibado

---
+++ docs/precache-manifest.2b08cdcb4b1fdb36da39453f0e390a33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdbf632cf12e8ce1f51c27bbd65b5b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19071f98df33c666c5ff",
+    "url": "/static/css/main.440aa28f.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19071f98df33c666c5ff",
+    "url": "/static/js/main.bc2f52e5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
